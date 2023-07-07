6fb9dc7a6efff2a4c6de3d063d56f6678c0dc9a4
Malique Hammonds <107649903+Leekasaurus@users.noreply.github.com>
2023-07-07 12:34:38 -0400

    hopefully finale

--- Color Picker/src/App.jsx
+++ Color Picker/src/App.jsx
@@ -1,15 +1,41 @@
-// Write your Color component here
+import { useState } from 'react'
+import './index.css'
 
-const App = () => {
-  return (
+
+// Write your Color component here
+  const Color = ({color, setSelectedColor}) => {
+    return(
+      <div className={color}   onClick={() => setSelectedColor(color)}></div>
+    
+      )
+    }
+    //I can still see you code <.<
+    function App() {
+      const [selectedColor, setSelectedColor] = useState("0")
+      console.log(useState(0));
+      const handleClick = () => {
+        setSelectedColor(color)
+      };
+      return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
+        <button onClick={handleClick}>{}color</button>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      
+       
+        <div id="colors-list">
+    <Color color = "violet" setSelectedColor = {setSelectedColor}/>
+    <Color color = "blue"  setSelectedColor = {setSelectedColor}/>
+    <Color color = "green" setSelectedColor = {setSelectedColor}/>
+    
+  </div>
     </div>
   );
-};
- 
+    }
+   
+
+
+
 export default App;
